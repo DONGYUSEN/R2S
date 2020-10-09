82f399c6cf63f74b15d96b715125b1c3af1d5f9c
zming66 <50068931+zming66@users.noreply.github.com>
2020-10-09 12:24:58 +0800

    Create 06_cleaning.sh

---
+++ SCRIPTS/06_cleaning.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm -rf `/bin/ls | grep -v -E '(squashfs|manifest)'`
+gzip -d *.gz
+gzip --best --keep *.img
+sha256sum openwrt* | tee sha256_$(date "+%Y%m%d").hash
+md5sum    openwrt* | tee    md5_$(date "+%Y%m%d").hash
+rm -f *.img
+exit 0
