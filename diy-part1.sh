81e51834c46230982f0134266f921bab5a603cb0
zming66 <50068931+zming66@users.noreply.github.com>
2020-10-25 12:07:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,3 +14,12 @@ rm -rf openwrt-chinadns-ng/ && git clone https://github.com/pexcn/openwrt-chinad
 git clone https://github.com/cnzd/luci-app-koolproxyR.git         #去广告最新KPR
 git clone https://github.com/destan19/OpenAppFilter.git           #APP过滤应用
 git clone https://github.com/jerrykuku/luci-theme-argon.git       #取消注释下载最新的argon主题
+git clone https://github.com/kongfl888/luci-app-minieap.git       #MiniEAP
+git clone https://github.com/lisaac/luci-plugin-samba.git         #网络共享
+git clone https://github.com/destan19/OpenAppFilter.git           #上网行为管理
+git clone https://github.com/kongfl888/luci-app-r2sflasher.git    #r2s图形化刷机
+git clone https://github.com/kongfl888/luci-app-wrtbwmon.git      #带宽跟踪器
+git clone https://github.com/kongfl888/luci-app-adguardhome.git   #adguardhome
+git clone https://github.com/kongfl888/luci-theme-argon.git       #界面   make menuconfig #choose LUCI->Theme->Luci-theme-argon 
+git clone https://github.com/kongfl888/luci-app-kcpufreq.git      #CPU性能优化
+git clone https://github.com/kongfl888/luci-app-koolproxyR.git    #koolproxyR
