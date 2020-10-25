f8f5adc39bb8119ce8e30d18cb80522eccd8a41e
zming66 <50068931+zming66@users.noreply.github.com>
2020-10-25 08:46:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,5 +12,5 @@ rm -rf openwrt-chinadns-ng/ && git clone https://github.com/pexcn/openwrt-chinad
 
 #添加额外软件包
 git clone https://github.com/cnzd/luci-app-koolproxyR.git         #去广告最新KPR
-#git clone https://github.com/destan19/OpenAppFilter.git           #APP过滤应用
-#git clone https://github.com/jerrykuku/luci-theme-argon.git       #取消注释下载最新的argon主题
+git clone https://github.com/destan19/OpenAppFilter.git           #APP过滤应用
+git clone https://github.com/jerrykuku/luci-theme-argon.git       #取消注释下载最新的argon主题
