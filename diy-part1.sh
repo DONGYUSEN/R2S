49195efb572fe5ba000f23cd35de6cd17448997a
zming66 <50068931+zming66@users.noreply.github.com>
2020-10-26 16:39:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,13 +12,10 @@ rm -rf openwrt-chinadns-ng/ && git clone https://github.com/pexcn/openwrt-chinad
 
 #添加额外软件包
 git clone https://github.com/cnzd/luci-app-koolproxyR package/luci-app-koolproxyR      #去广告最新KPR
-#git clone https://github.com/destan19/OpenAppFilter                                   #APP过滤应用
-#git clone https://github.com/jerrykuku/luci-theme-argon                               #取消注释下载最新的argon主题
 git clone https://github.com/kongfl888/luci-app-minieap package/luci-app-minieap       #MiniEAP
 git clone https://github.com/lisaac/luci-plugin-samba package/luci-plugin-samba        #网络共享
 git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter              #上网行为管理
 git clone https://github.com/kongfl888/luci-app-r2sflasher package/luci-app-r2sflasher #r2s图形化刷机
 git clone https://github.com/kongfl888/luci-app-wrtbwmon package/luci-app-wrtbwmon     #带宽跟踪器
-#git clone https://github.com/kongfl888/luci-theme-argon       #界面   make menuconfig #choose LUCI->Theme->Luci-theme-argon 
 git clone https://github.com/kongfl888/luci-app-kcpufreq package /luci-app-kcpufreq    #CPU性能优化
 
