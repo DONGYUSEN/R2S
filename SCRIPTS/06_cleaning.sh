a1b566816642c4fe0c37c747ab2b528352788f27
zming66 <50068931+zming66@users.noreply.github.com>
2020-11-01 09:38:27 +0800

    Update 06_cleaning.sh

--- SCRIPTS/06_cleaning.sh
+++ SCRIPTS/06_cleaning.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-rm -rf `/bin/ls | grep -v -E '(squashfs|manifest)'`
+rm -rf `ls | grep -v "squashfs"`
 gzip -d *.gz
-gzip --best --keep *.img
-sha256sum openwrt* | tee sha256_$(date "+%Y%m%d").hash
-md5sum    openwrt* | tee    md5_$(date "+%Y%m%d").hash
-rm -f *.img
+gzip *.img
 exit 0
